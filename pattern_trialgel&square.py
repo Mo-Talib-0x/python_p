bad2e7f7608fbad898bf02e74aa84117395c68e6
Mo-Talib-0x <motalib8099@gmail.com>
2025-07-13 15:47:06 +0530

    square and triangle pattern

---
+++ pattern_trialgel&square.py
@@ -0,0 +1,7 @@
+rows=int(input("Enter the number of rows you want: "))
+for i in range(1,rows+1):
+    print("*"*i)
+
+rows=int(input("Enter the number of rows you want: "))
+for i in range(1,rows+1):
+    print("*"*rows)
